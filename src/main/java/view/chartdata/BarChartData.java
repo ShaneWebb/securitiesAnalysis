ba76973969ee91a4d384f14c420a5ff49e96a9f0
Shane Webb <webb@singularmeans.com>
2020-08-29 09:18:02 -0700

    Barchart now sorts the bins.

--- src/main/java/view/chartdata/BarChartData.java
+++ src/main/java/view/chartdata/BarChartData.java
@@ -2,7 +2,10 @@ package view.chartdata;
 
 import java.text.ParseException;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import javautilwrappers.ArrayListWrapper;
 import javautilwrappers.HashMapWrapper;
 import javautilwrappers.ListWrapper;
@@ -34,12 +37,11 @@ public class BarChartData extends AbstractBinnedData {
         ListWrapper<MapWrapper<String, Object>> internalSubData
                 = (ListWrapper<MapWrapper<String, Object>>) data.unwrap();
 
-        
         RangeFinder finder = rangeFinderFactory(internalSubData);
         MapWrapper<String, Integer> rangesMap = new HashMapWrapper<>();
         for (MapWrapper<String, Object> item : internalSubData) {
-            String range = finder.getRange((int)((double)item.get("value")));
-            if(range == null) {
+            String range = finder.getRange((int) ((double) item.get("value")));
+            if (range == null) {
                 //The mathematical calculation will very rarely fail to bin
                 //a value due to rounding error.
                 continue;
@@ -52,18 +54,30 @@ public class BarChartData extends AbstractBinnedData {
             }
         }
         
+        Comparator<String> comparator = (String o1, String o2) -> {
+            String[] o1delimited = o1.split(" ");
+            String[] o2delimited = o2.split(" ");
+            Integer i1 = Integer.parseInt(o1delimited[0]);
+            Integer i2 = Integer.parseInt(o2delimited[0]);
+            return i1.compareTo(i2);
+        };
+        
+        SortedSet<String> keys = new TreeSet<>(comparator);
+        keys.addAll(rangesMap.unwrap().keySet());
+
         ListWrapper<MapWrapper<String, Object>> processedSubData
                 = new ArrayListWrapper<>();
-        
-        String fileName = ((BarChartSubData) data).getFileName();
-        for(MapWrapper.Entry<String, Integer> entry: rangesMap.entrySet()) {
+
+        String fileName = ((AbstractBinnedSubData) data).getFileName();
+
+        for (String key: keys) {
             MapWrapper<String, Object> tempMap = new HashMapWrapper<>();
-            tempMap.put("row", entry.getKey());
+            tempMap.put("row", key);
             tempMap.put("col", fileName);
-            tempMap.put("value", entry.getValue());
+            tempMap.put("value", rangesMap.get(key));
             processedSubData.add(tempMap);
         }
-
+        
         for (MapWrapper<String, Object> item : processedSubData) {
             this.internalDataset.addValue(
                     (Integer) item.get("value"),
--- src/test/java/learner/java/MapTest.java
+++ src/test/java/learner/java/MapTest.java
@@ -1,13 +1,16 @@
 package learner.java;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class MapTest {
-    
+
     public MapTest() {
     }
 
@@ -15,11 +18,37 @@ public class MapTest {
     //@Disabled
     public void hashMapTest() {
         Map<Integer, String> myMap = new HashMap<>();
-        myMap.put(1,"Hello");
+        myMap.put(1, "Hello");
         myMap.put(2, "World");
         assertEquals("Hello", myMap.get(1), "Map retrieval should work.");
         assertEquals("World", myMap.get(2), "Map retrieval should work.");
-        assertEquals(null,myMap.get(3), "Should return null as key 3 does not exist");
+        assertEquals(null, myMap.get(3), "Should return null as key 3 does not exist");
     }
-    
+
+    @Test
+    public void treeMapTest() {
+
+        Comparator<String> comparator = (String o1, String o2) -> {
+                String[] o1delimited = o1.split(" ");
+                String[] o2delimited = o2.split(" ");
+                Integer i1 = Integer.parseInt(o1delimited[0]);
+                Integer i2 = Integer.parseInt(o2delimited[0]);
+                return i1.compareTo(i2);
+            };
+                
+        Map<String, Integer> someMap = new HashMap<>();
+        someMap.put("100 - 210", 3);
+        someMap.put("10 - 20", 1);
+        someMap.put("50 - 60", 2);
+        
+        SortedSet<String> keys = new TreeSet<>(comparator);
+        
+        keys.addAll(someMap.keySet());
+
+        for (String key : keys) {
+            Integer value = someMap.get(key);
+            System.out.println(value);
+        }
+    }
+
 }
