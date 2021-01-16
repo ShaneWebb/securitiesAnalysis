f9ff1411f751548e1288ce81e4a59a7a81ecfd1a
Shane <shane@Shane-Workstation>
2021-01-16 14:16:26 -0700

    Some learner tests created for working with dates and other queries.

--- src/test/java/learner/h2/H2DBTest.java
+++ src/test/java/learner/h2/H2DBTest.java
@@ -1,37 +1,101 @@
 package learner.h2;
 
 import java.sql.*;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class H2DBTest {
 
-    private static Connection conn;
+    private Connection inMemConn;
+    private Statement stmt;
 
-    @BeforeAll
-    public static void createDB() throws Exception {
-        conn = DriverManager.getConnection("jdbc:h2:mem:", "sa", "");
+    @BeforeEach
+    public void createDB() throws Exception {
+        inMemConn = DriverManager.getConnection("jdbc:h2:mem:");
+        stmt = inMemConn.createStatement();
     }
 
-    @AfterAll
-    public static void closeDB() throws Exception {
-        conn.close();
+    @AfterEach
+    public void closeDB() throws Exception {
+        inMemConn.close();
     }
 
     @Test
     public void basicTest() {
-        try {
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT 1+1");
+        try ( ResultSet rs = stmt.executeQuery("SELECT 1+1")) {
             if (rs.next()) {
                 assertEquals(rs.getInt(1), 2);
+            } else {
+                fail("Nothing in the resultset!");
             }
-        }
-        catch (SQLException ex) {
+        } catch (SQLException ex) {
             fail(ex.getMessage());
+        } finally {
+        }
+    }
+
+    @Test
+    public void queryTests() throws Exception {
+        String sql = "CREATE TABLE REGISTRATION "
+                + "(id INTEGER not NULL, "
+                + " first VARCHAR(255), "
+                + " last VARCHAR(255), "
+                + " age INTEGER, "
+                + " PRIMARY KEY ( id ))";
+        stmt.executeUpdate(sql);
+
+        String sqlInsert = "INSERT INTO REGISTRATION "
+                + "VALUES (100, 'Zara', 'Ali', 18)";
+        stmt.executeUpdate(sqlInsert);
+
+        String sqlSelect = "SELECT first FROM REGISTRATION "
+                + "WHERE id = 100";
+        ResultSet rs1 = stmt.executeQuery(sqlSelect);
+
+        if (rs1.next()) {
+            assertEquals("Zara", rs1.getString("first"));
+        } else {
+            fail();
+        }
+
+        String sqlSelectMore = "Select first, last, age FROM Registration "
+                + "where id = 100";
+        ResultSet rs2 = stmt.executeQuery(sqlSelectMore);
+
+        if (rs2.next()) {
+            assertEquals("Ali", rs2.getString("last"));
+            assertEquals("18", rs2.getString("age"));
+            assertEquals(18, rs2.getInt("age"));
+        } else {
+            fail();
+        }
+
+    }
+
+    @Test
+    public void dateTest() throws Exception {
+        String sql = "CREATE TABLE sample "
+                + "(id DATE not NULL, "
+                + " open BIGINT, "
+                + " PRIMARY KEY ( id ))";
+        stmt.executeUpdate(sql);
+
+        String sqlInsert = "Insert into sample "
+                + "VALUES (?, 4000)";
+        PreparedStatement pstmt = inMemConn.prepareStatement(sqlInsert);
+        pstmt.setDate(1, Date.valueOf("1992-06-01"));
+        pstmt.execute();
+        
+        String sqlSelect = "Select * from sample";
+        ResultSet rs = stmt.executeQuery(sqlSelect);
+        
+        if(rs.next()) {
+            assertEquals(Date.valueOf("1992-06-01"), rs.getDate("id"));
+        } else {
+            fail();
         }
     }
 
